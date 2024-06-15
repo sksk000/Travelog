c46c858079e515dcd9a8b4eb2ba18c055341c37f
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2024-06-15 13:59:28 +0000

    [add]aboutページを表示、ユーザ認証のルートやaboutページのルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'homes/top'
+  scope module: :public do
+    devise_for :users
+    root to: 'homes#top'
+    get 'homes/about', to: 'homes#about', as: :about
+    get 'homes/about' => 'homes#about'
+    get 'users/mypage' => 'users#mypage', as: :mypage
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
