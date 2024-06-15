e39340e3dcc3aa4dcece7536341ee00cf7064561
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2024-06-15 14:03:51 +0000

    [add]管理者用のユーザ認証処理の各ファイルを作成、devise作成時に生成されたファイルを追加

---
+++ app/controllers/public/users_controller.rb
@@ -0,0 +1,23 @@
+class Public::UsersController < ApplicationController
+
+  # マイページ
+  def mypage
+  end
+
+  # マイページ編集
+  def edit
+  end
+
+  # ユーザ詳細ページ
+  def show
+  end
+
+  # ユーザ情報編集
+  def update
+  end
+
+  # ユーザ退会
+  def withdraw
+  end
+
+end
