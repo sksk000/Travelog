46c122e6a1178ce1c1c0f31b455a5a8b68733377
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2024-06-18 13:18:17 +0000

    [add]postのDB設定完了

--- db/migrate/20240618123038_create_posts.rb
+++ db/migrate/20240618123038_create_posts.rb
@@ -1,7 +1,12 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-
+      t.string :title
+      t.string :body
+      t.boolean :is_release
+      t.boolean :is_stoprelease
+      t.references :user, foreign_key: true
+      t.integer :spot, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_15_130747) do
+ActiveRecord::Schema.define(version: 2024_06_18_123038) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.boolean "is_release"
+    t.boolean "is_stoprelease"
+    t.integer "user_id"
+    t.integer "spot"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +39,5 @@ ActiveRecord::Schema.define(version: 2024_06_15_130747) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
