55afc94e59b468d610e91f405443b3610ab4a567
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2024-06-18 12:35:20 +0000

    [add]postモデルとcontroller作成

---
+++ app/controllers/public/posts_controller.rb
@@ -0,0 +1,19 @@
+class Public::PostsController < ApplicationController
+  def create
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
