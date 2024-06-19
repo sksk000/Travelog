f0b5871949d5c3fbfa30b590d6b852bef0493b2d
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2024-06-19 14:10:10 +0000

    　[add]投稿削除機能作成,アラートダイアログにて削除するか確認後に削除を行えるように実装

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -33,6 +33,9 @@ class Public::PostsController < ApplicationController
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to mypage_path
   end
 
   def show
