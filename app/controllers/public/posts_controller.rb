ba12ffd6d678f040cb3e2ca880a23b9c89e69920
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2024-06-19 13:47:22 +0000

    [update]投稿編集実装

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -18,9 +18,18 @@ class Public::PostsController < ApplicationController
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to mypage_path
+    else
+      flash.now[:alert] = @post.errors.full_messages.join(', ')
+      render :edit
+    end
+
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def destroy
