fae7278e8690ac6abcd4851ea4f4b614e309bdd7
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2024-06-19 13:25:57 +0000

    [update]新規投稿実装

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,11 +1,20 @@
 class Public::PostsController < ApplicationController
   def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to mypage_path
+    else
+      flash.now[:alert] = @post.errors.full_messages.join(', ')
+      render :new
+    end
   end
 
   def index
   end
 
   def new
+    @post = Post.new
   end
 
   def update
@@ -15,5 +24,10 @@ class Public::PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
       delete :withdraw, on: :collection
     end
 
+    resources :posts, only:[:new, :index, :show, :create, :edit, :update, :destroy]
+
 
 
   end
