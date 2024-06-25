d2b425724156e883a62c7e14fd3e80ea207a3d69
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2024-06-25 14:01:28 +0000

    [add]テストデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+olivia = User.find_or_create_by!(email: "olivia@example.com") do |user|
+  user.name = "Olivia"
+  user.password = "password"
+end
+
+Post.find_or_create_by!(user: olivia) do |post_data|
+  post_data.user = olivia
+  post_data.title = "沖縄に来てみた"
+  post_data.body = "素敵な海を見れた！"
+end
+
+lucas = User.find_or_create_by!(email: "lucas@example.com") do |user|
+  user.name = "Lucas"
+  user.password = "password"
+end
+
+Post.find_or_create_by!(user: lucas) do |post_data|
+  post_data.user = lucas
+  post_data.title = "北海道に来てみた"
+  post_data.body = "海鮮が美味しかった"
+end
+
