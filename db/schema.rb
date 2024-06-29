8a785eafe01880ae008b82d7504701513d2b0693
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2024-06-29 04:33:14 +0000

    [add]comment用のmodelとcontroller作成、dbも登録完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_18_123038) do
+ActiveRecord::Schema.define(version: 2024_06_29_042016) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -39,5 +47,6 @@ ActiveRecord::Schema.define(version: 2024_06_18_123038) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "posts", "users"
 end
