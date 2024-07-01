0d78f82abac93f178338b8293b1e53fff4f7637c
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2024-07-01 12:55:52 +0000

    [add]コメント削除機能作成

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -12,6 +12,13 @@ class Public::CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @post = Post.find(params[:post_id])
+    @comment.destroy
+    redirect_to post_path(@post.id)
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:comment,:user_id,:post_id)
