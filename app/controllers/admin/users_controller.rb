bdcad77ba4d36382d52c692c952dd3f321d269fb
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2024-07-05 11:21:54 +0000

    [add]管理者ページにてユーザの削除機能追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,14 @@ class Admin::UsersController < ApplicationController
   def index
     @userall = User.all
   end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
