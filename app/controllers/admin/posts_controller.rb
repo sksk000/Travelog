a2678ea9c7bdd40041a2bcb581e4dfbb89c71893
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2024-07-15 09:13:33 +0000

    [add]Adminにて投稿記事一覧ページ作成,投稿の削除,投稿のコメント削除機能実装

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,15 @@
+class Admin::PostsController < ApplicationController
+  def index
+    @post = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id]);
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to admin_posts_path
+  end
+end
