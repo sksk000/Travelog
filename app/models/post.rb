17591bb015815712f84d717b234366ba5bb702b7
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2024-07-22 14:39:21 +0000

    [update]検索時に表示するタグ一覧をenumから参照するように修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,18 +9,22 @@ class Post < ApplicationRecord
   geocoded_by :address
   after_validation :geocode
 
-  enum season: { spring: 0, summer: 1, fall: 2, winter: 3 }
-  enum place: { abroad: 0, domestic: 1 }
-  enum night: { onenight: 0, twonight: 1, threenight: 2, fournight: 3, fivenight: 4 }
-  enum people: { oneperson: 0, twopersons: 1, threepersons: 2, fourpersons: 3, fivepersons: 4 }
+  enum season: { 春: 0, 夏: 1, 秋: 2, 冬: 3 }
+  enum place: { 海外旅行: 0, 国内旅行: 1 }
+  enum night: { 一泊: 0, 二泊: 1, 三泊: 2, 四泊: 3, 五泊: 4 }
+  enum people: { 一人: 0, 二人: 1, 三人: 2, 四人: 3, 五人: 4 }
 
 
-  def self.looks(searchdata,condition)
+  def self.looks(searchdata,condition,season,place,night,people)
     # 部分一致
     if condition == "PartialMatch"
       @post = Post.where("title LIKE?","%#{searchdata}%")
     elsif condition == "PerfectMatch"
       @post = Post.where(title: searchdata)
+    end
+
+    if season != "all_seasons"
+      
     end
   end
 end
