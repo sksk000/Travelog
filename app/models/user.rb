78c7b3b3a45d5fa3fc5ae6aa1273621143508657
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2024-07-24 14:24:11 +0000

    [add]ゲストログイン実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,13 @@ class User < ApplicationRecord
       @user = User.where(name: searchdata)
     end
   end
+
+  GUEST_USER_EMAIL = "guest@example.com"
+
+  def self.guest
+    find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
+  end
 end
