99b24d118d12c29881d3bca0fd13ed9ae4e56260
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2024-09-03 13:50:50 +0000

    [update]管理者ページの投稿詳細ページレイアウト修正、ユーザ詳細ページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     sessions: 'admin/sessions'
   }
   namespace :admin do
-    resources :users, only:[:index,:destroy]
+    resources :users, only:[:index,:show,:destroy]
     resources :posts, only:[:index,:show,:destroy]
     resources :comments, only:[:destroy]
   end
