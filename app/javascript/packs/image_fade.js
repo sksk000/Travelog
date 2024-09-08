52a529a9c0d7cd06e2d26009d131dcc8a4396292
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2024-09-08 06:32:01 +0000

    [update]topページの背景をフェードできるように修正

--- app/javascript/packs/image_fade.js
+++ app/javascript/packs/image_fade.js
@@ -1,3 +1,30 @@
-var imgpath = []
-var fadespeed = 4000
+var timer = 3000;
 
+// すべての画像を取得
+var imgs = document.querySelectorAll("#slider img");
+
+//一番最初の画像を表示する
+imgs[0].classList.add('show');
+var num = 0;
+
+function showImg () {
+  //表示していた画像を非表示にする
+  imgs[num].classList.remove('show');
+
+  //次の画像番号を設定
+  if (num >= imgs.length - 1) {
+    num = 0;
+  } else {
+    num++;
+  }
+
+  //表示設定
+  imgs[num].classList.add('show');
+  showImgTimer();
+}
+function showImgTimer () {
+  setTimeout(showImg, timer);
+}
+
+
+showImgTimer();
