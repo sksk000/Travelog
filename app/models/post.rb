f40e3d57fde881d3f7ffbd0dd0ed484f7576526a
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2024-12-17 15:25:24 +0000

    [update]削除時にpostに紐づいているテーブルにdependent: :destroyをつけていない物があるので、つけるように変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,9 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :places, dependent: :destroy
   has_one_attached :image, dependent: :destroy
-  has_many :post_tags
+  has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
-  has_many :post_prefectures
+  has_many :post_prefectures, dependent: :destroy
 
 
   validates :title, presence: true
