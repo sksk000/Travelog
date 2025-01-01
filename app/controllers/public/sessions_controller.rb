99aa63973386fde12f1e7a010f76260d82694752
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2025-01-01 13:26:39 +0000

    [update]ユーザ認証周りのエラー・成功メッセージをトーストで表示するように修正

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -40,6 +40,7 @@ class Public::SessionsController < Devise::SessionsController
 
     if resource.valid_password?(params[:user][:password])
       sign_in(scope, resource)
+      flash[:notice] = "ログインに成功しました。"
       redirect_to after_sign_in_path_for(resource)
     else
       flash[:alert] = "メールアドレスまたはパスワードが間違っています。"
