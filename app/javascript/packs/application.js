b0b15ca4151aaad23db6a78fe1dca88bbb2f8d0d
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2025-01-01 10:23:26 +0000

    [add]toastrプラグイン導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ import "bootstrap";
 import "../stylesheets/application";
 import "controllers"
 import $ from 'jquery';
+import toastr from 'toastr';
 
 Rails.start()
 Turbolinks.start()
@@ -77,3 +78,4 @@ document.addEventListener('turbolinks:load', function() {
 });
 
 window.$ = window.jQuery = require('jquery');
+window.toastr = toastr;
