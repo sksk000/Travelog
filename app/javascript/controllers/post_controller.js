ae169ea49d28c3eeae2719ae2409691172a981fc
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2025-01-02 07:42:44 +0000

    [add]タグのバリデーション処理を追加

--- app/javascript/controllers/post_controller.js
+++ app/javascript/controllers/post_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
+import { validPostForm } from "../packs/validation";
 
 // ブートストラップ ローダ
 (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
@@ -131,7 +132,7 @@ export default class extends Controller {
     e.preventDefault()
     const formData = new FormData(this.formTarget)
 
-    if(!this.validateForm()){
+    if(!validPostForm()){
       this.submitTarget.blur()
       return
     }
@@ -227,33 +228,6 @@ export default class extends Controller {
       return
     });
 
-  }
-
-   validateForm(){
-
-    let errormsg = ""
-    let isSuccess = true
-
-    const title = this.formTarget.querySelector('[name="post[title]"]');
-    const body = this.formTarget.querySelector('[name="post[body]"]');
-
-
-    // タイトルと投稿内容が空でないかチェック
-    if(!title.value.trim()){
-      errormsg = errormsg + "タイトルが空欄です\n"
-      isSuccess = isSuccess && false
-    }
-
-    if (!body.value.trim()) {
-      errormsg = errormsg + "投稿内容が空欄です\n"
-      isSuccess = isSuccess && false
-    }
-
-    if(isSuccess){
-      return true
-    } else {
-      alert(errormsg)
-    }
   }
 
   moveMaker(e){
