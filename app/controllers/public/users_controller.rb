1328569b37b90883a3bfb2da0db54a72586eb31f
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2025-01-24 08:32:15 +0000

    [update]マイページの自身の投稿のソート処理をUserモデルに移動

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -46,15 +46,7 @@ class Public::UsersController < ApplicationController
 
   def post_index
     @user = User.find(params[:id])
-
-    @posts = case params[:select]
-             when 'newpost'
-               @user.posts.order(created_at: :desc)
-             when 'comment'
-               @user.posts.left_joins(:comments).group(:id).order('COUNT(comments.id) DESC')
-             else
-               @user.posts
-             end
+    @posts = @user.getOrderUserPostData(params[:select])
 
     render partial: 'public/posts/index', layout: false, locals: { targetpost: @posts, userdata: @user }
   end
