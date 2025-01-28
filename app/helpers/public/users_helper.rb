859305f485957c5f09659c7a881e5d154dd290c3
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2025-01-28 06:17:44 +0000

    [update]マイページのViewをリファクタリング

--- app/helpers/public/users_helper.rb
+++ app/helpers/public/users_helper.rb
@@ -1,2 +1,22 @@
 module Public::UsersHelper
+  def user_index_image(user, size, options = {})
+    options[:class] ||= "card-img-top"
+    options[:size] = size
+
+    if user.profile_image.present?
+      image_tag(user.profile_image, options)
+    else
+      image_tag(asset_path('no_image.jpg'), options)
+    end
+  end
+
+  def user_profile_image(user, size, options = {})
+    options[:size] = size
+    if user.profile_image.present?
+      image_tag(rails_blob_path(user.profile_image), options)
+    else
+      image_tag(asset_path('no_image.jpg'), options)
+    end
+  end
+
 end
