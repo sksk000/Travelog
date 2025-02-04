e4fc4163e3fb034ebfc92f2973bad99f393432e4
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2025-02-04 06:13:24 +0000

    [update]投稿一覧のタグを選択時にタグ検索を行えるように実装

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,13 +1,15 @@
 class Public::SearchesController < ApplicationController
   def search
     @category = params[:search_category_select]
-    @searchdata = search_params[:seachdata]
+    @searchdata = search_params[:searchdata]
 
     case @category
     when "user_search"
       @results = User.looks(search_params[:searchdata])
     when "post_search"
       @results = Post.looks(search_params)
+    when "tag_search"
+      @results = Tag.looks(search_params[:searchdata])
     end
 
     @is_empty = @results.empty?
@@ -24,14 +26,6 @@ class Public::SearchesController < ApplicationController
   private
 
   def search_params
-    {
-      searchdata: params[:seachdata] || '',
-      travel_months: params[:travel_months],
-      prefectures: params[:prefectures],
-      stay_nights: params[:stay_nights],
-      people: params[:people],
-      post_months: params[:post_months] || nil,
-      travel_goods: params[:travel_goods] || nil
-    }
+     params.permit(:searchdata, :travel_months, :prefectures, :stay_nights, :people, :post_months, :travel_goods)
   end
 end
